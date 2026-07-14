408dadd7fdf7f3facbc99d5c7f909ccf68a2c6cf
Hyunwoo Nam <skagusdn1998@gmail.com>
2026-07-14 14:59:19 +0900

    Add evidence bundle artifact metadata

--- velune_trace/reporting/__init__.py
+++ velune_trace/reporting/__init__.py
@@ -1,5 +1,9 @@
 """Evidence Report Bundle construction helpers."""
 
+from velune_trace.reporting.artifacts import build_artifact_record
 from velune_trace.reporting.manifest import build_private_report_manifest
 
-__all__ = ["build_private_report_manifest"]
+__all__ = [
+    "build_artifact_record",
+    "build_private_report_manifest",
+]
