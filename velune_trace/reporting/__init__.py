be8375d126d48e12139437f621e4f29eec3b4dd3
Hyunwoo Nam <skagusdn1998@gmail.com>
2026-07-14 16:46:35 +0900

    Add content-derived evidence bundle identity

--- velune_trace/reporting/__init__.py
+++ velune_trace/reporting/__init__.py
@@ -1,9 +1,11 @@
 """Evidence Report Bundle construction helpers."""
 
 from velune_trace.reporting.artifacts import build_artifact_record
+from velune_trace.reporting.identity import build_report_bundle_id
 from velune_trace.reporting.manifest import build_private_report_manifest
 
 __all__ = [
     "build_artifact_record",
     "build_private_report_manifest",
+    "build_report_bundle_id",
 ]
